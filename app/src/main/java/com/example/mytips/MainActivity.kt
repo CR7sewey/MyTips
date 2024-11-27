e3938e4c862b2bb04d23b854a95a4a5273c926a1
CR7sewey <miguelmotacarvalho@gmail.com>
2024-11-27 08:53:18 +0000

    Layout and Logic - Part 2

--- app/src/main/java/com/example/mytips/MainActivity.kt
+++ app/src/main/java/com/example/mytips/MainActivity.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintSet.Layout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.textfield.TextInputEditText
@@ -31,47 +30,121 @@ class MainActivity : AppCompatActivity() {
         val minus = findViewById<TextView>(R.id.minus)
         val plus = findViewById<TextView>(R.id.plus)
         val valueSplit = findViewById<TextView>(R.id.valueSplit)
+        val tip = findViewById<View>(R.id.tip)
+        val tip0 = findViewById<View>(R.id.tip11)
+        val tip10 = findViewById<View>(R.id.tip12)
+        val tip20 = findViewById<View>(R.id.tip13)
+        val tip25 = findViewById<View>(R.id.tip21)
+        val tip30 = findViewById<View>(R.id.tip22)
+        val billEnd = findViewById<TextView>(R.id.billFinal)
+        val tipEnd = findViewById<TextView>(R.id.tipFinal)
+        var tipFinal: Double = 0.0
+
         //var finalValuePreUpdate = 0.0
 
-        mainActivity.setOnClickListener {
-            val finalValuePreUpdateBD = calculateTotalPerPerson(billTotal, valueSplit)
-            finalValue.text = "€ " + finalValuePreUpdateBD
-        }
+
+
         billTotal.setOnClickListener {
-            println(billTotal.id.compareTo(billTotal.id))
-            println(billTotal.id.compareTo(minus.id))
+            //println(billTotal.id.compareTo(billTotal.id))
+            //println(billTotal.id.compareTo(minus.id))
 
             //finalValuePreUpdate = billTotal.text.toString().toDouble() / valueSplit.text.toString().toDouble()
-            val finalValuePreUpdateBD = calculateTotalPerPerson(billTotal, valueSplit)
-            finalValue.text = "€ " + finalValuePreUpdateBD
+            val finalValuePreUpdateBD = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[2]
+            val tipShown = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[0]
+            val finalValuePreUpdate = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[1]
+            finalValue.text = "€ $finalValuePreUpdateBD"
+            tipEnd.text = "€$tipShown"
+            billEnd.text = "€$finalValuePreUpdate"
         }
 
         minus.setOnClickListener {
             if (valueSplit.text.toString().toInt() > 1) {
                 valueSplit.text = (valueSplit.text.toString().toInt() - 1).toString()
-                val finalValuePreUpdateBD = calculateTotalPerPerson(billTotal, valueSplit)
-                finalValue.text = "€ " + finalValuePreUpdateBD
+                val finalValuePreUpdateBD = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[2]
+                val tipShown = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[0]
+                val finalValuePreUpdate = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[1]
+                finalValue.text = "€ $finalValuePreUpdateBD"
+                tipEnd.text = "€$tipShown"
+                billEnd.text = "€$finalValuePreUpdate"
             }
         }
         plus.setOnClickListener {
             if (valueSplit.text.toString().toInt() < 20) {
                 valueSplit.text = (valueSplit.text.toString().toInt() + 1).toString()
-                val finalValuePreUpdateBD = calculateTotalPerPerson(billTotal, valueSplit)
-                finalValue.text = "€ " + finalValuePreUpdateBD
+                val finalValuePreUpdateBD = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[2]
+                val tipShown = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[0]
+                val finalValuePreUpdate = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[1]
+                finalValue.text = "€ $finalValuePreUpdateBD"
+                tipEnd.text = "€$tipShown"
+                billEnd.text = "€$finalValuePreUpdate"
             }
         }
 
 
+        mainActivity.setOnClickListener {
 
+            val finalValuePreUpdateBD = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[2]
+            val tipShown = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[0]
+            val finalValuePreUpdate = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[1]
+            finalValue.text = "€ $finalValuePreUpdateBD"
+            tipEnd.text = "€$tipShown"
+            billEnd.text = "€$finalValuePreUpdate"
+        }
 
-
+        tip0.setOnClickListener {
+            tipFinal = 0.0
+            val finalValuePreUpdateBD = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[2]
+            val tipShown = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[0]
+            val finalValuePreUpdate = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[1]
+            finalValue.text = "€ $finalValuePreUpdateBD"
+            tipEnd.text = "€$tipShown"
+            billEnd.text = "€$finalValuePreUpdate"
+        }
+        tip10.setOnClickListener {
+            tipFinal = 0.1
+            val finalValuePreUpdateBD = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[2]
+            val tipShown = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[0]
+            val finalValuePreUpdate = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[1]
+            finalValue.text = "€ $finalValuePreUpdateBD"
+            tipEnd.text = "€$tipShown"
+            billEnd.text = "€$finalValuePreUpdate"
+        }
+        tip20.setOnClickListener {
+            tipFinal = 0.2
+            val finalValuePreUpdateBD = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[2]
+            val tipShown = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[0]
+            val finalValuePreUpdate = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[1]
+            finalValue.text = "€ $finalValuePreUpdateBD"
+            tipEnd.text = "€$tipShown"
+            billEnd.text = "€$finalValuePreUpdate"
+        }
+        tip25.setOnClickListener {
+            tipFinal = 0.25
+            val finalValuePreUpdateBD = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[2]
+            val tipShown = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[0]
+            val finalValuePreUpdate = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[1]
+            finalValue.text = "€ $finalValuePreUpdateBD"
+            tipEnd.text = "€$tipShown"
+            billEnd.text = "€$finalValuePreUpdate"
+        }
+        tip30.setOnClickListener {
+            tipFinal = 0.3
+            val finalValuePreUpdateBD = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[2]
+            val tipShown = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[0]
+            val finalValuePreUpdate = calculateTotalPerPerson(billTotal, valueSplit, tipFinal)[1]
+            finalValue.text = "€ $finalValuePreUpdateBD"
+            tipEnd.text = "€$tipShown"
+            billEnd.text = "€$finalValuePreUpdate"
+        }
 
     }
 
-    private fun calculateTotalPerPerson(billTotal: TextInputEditText, valueSplit: TextView): String {
-        val finalValuePreUpdate = billTotal.text.toString().toDouble() / valueSplit.text.toString().toDouble()
-        val finalValuePreUpdateBD = (BigDecimal(finalValuePreUpdate).setScale(2, RoundingMode.HALF_EVEN)).toString()
-        return finalValuePreUpdateBD
+    private fun calculateTotalPerPerson(billTotal: TextInputEditText, valueSplit: TextView, tipFinal: Double): Array<String> {
+        println("AQUIIIII")
+        val tip = (BigDecimal(billTotal.text.toString().toDouble() / valueSplit.text.toString().toDouble() * tipFinal).setScale(2, RoundingMode.HALF_EVEN)).toString()
+        val finalValuePreUpdate = (BigDecimal(billTotal.text.toString().toDouble() / valueSplit.text.toString().toDouble()).setScale(2,RoundingMode.HALF_EVEN)).toString()
+        val finalValuePreUpdateBD = (BigDecimal(finalValuePreUpdate.toDouble() + tip.toDouble()).setScale(2, RoundingMode.HALF_EVEN)).toString()
+        return arrayOf(tip, finalValuePreUpdate, finalValuePreUpdateBD)
 
     }
 }
